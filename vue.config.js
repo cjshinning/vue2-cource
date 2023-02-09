9d8e6ee3a53cb85804b112cdad1075395a5c1b7b
cjshinningg <784319417@qq.com>
2023-02-09 21:46:05 +0800

    1、插件配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path');
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [path.resolve(__dirname, 'src/assets/common.scss')]
+    }
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        postcssOptions: {
+          plugins: [
+            require('postcss-plugin-px2rem')({
+              rootValue: 37.5,  //表示设计稿的大小 375->37.5rem
+              // exclude: /node_modules/
+            })
+          ]
+        }
+      }
+    }
+  }
 })
